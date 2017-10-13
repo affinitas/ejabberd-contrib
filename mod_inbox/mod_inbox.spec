026ce2262fff75436f374bec349a0abdd11ba43e
Alexey Dmitriev <alexey.dmitriev@affinitas.de>
2017-10-13 14:00:01 +0200

    add mod_inbox

---
+++ mod_inbox/mod_inbox.spec
@@ -0,0 +1,5 @@
+author: "Hamdullah Shah <hamdullah.shah@affinitas.de>"
+category: "command"
+summary: "Fetch a specific messages from archive"
+home: "https://github.com/affinitas/ejabberd-contrib/tree/master/"
+url: "https://github.com/affinitas/ejabberd-contrib/archive/master.zip"
---
+++ mod_inbox/src/mod_inbox.erl
@@ -0,0 +1,88 @@
+%%%-------------------------------------------------------------------
+%%% @author Hamdullah Shah
+%%% ©copyright (C) 2017-2018, Affinitas.de
+%%% @doc
+%%%
+%%%
+%%% @end
+%%% Create: 2 October 2017
+%%%-------------------------------------------------------------------
+
+-module(mod_inbox).
+-author("Hamdullah Shah").
+
+-behavior(gen_mod).
+-include("ejabberd.hrl").
+-include("xmpp.hrl").
+-include("logger.hrl").
+-include("ejabberd_commands.hrl").
+-include("ejabberd_sql_pt.hrl").
+-include("mod_mam.hrl").
+
+-export([start/2,
+	 stop/1,
+	 mod_opt_type/1,
+   get_inbox/2,
+	 depends/2]).
+
+
+start(Host, Opts) ->
+  ejabberd_commands:register_commands(get_commands_spec()).
+
+stop(Host) ->
+    ejabberd_commands:unregister_commands(get_commands_spec()).
+
+get_commands_spec() ->
+  [#ejabberd_commands{name = get_inbox, tags = [purge],
+      desc = "Latest message in each unique conversation",
+      longdesc = "It return the latest messages with each user",
+      module = ?MODULE, function = get_inbox,
+      args = [{host, binary},{user, binary}],
+      args_example = ["localhost","user1"],
+      result = {messages,string},
+      result_desc = "Array of JSON: Latest message from each conversation"}].
+
+
+get_inbox(Host,User) ->
+  DB_Type = gen_mod:db_type(Host, mod_mam),
+  fetch_inbox(Host,User,DB_Type).
+
+mod_opt_type(_) ->
+	[].
+
+depends(_,_) ->
+	[].
+
+
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+fetch_inbox(Host, User,  sql) ->
+  Query = makeQuery(User),
+  Messges = case ejabberd_sql:sql_query(Host,Query) of
+        {selected,Columns,Rest} ->
+          % {[[binary_to_list(X) || X <- Row] || Row <- Rest]};
+          RowDic=[{lists:zip(Columns,Row)} || Row <- Rest],
+          jiffy:encode(RowDic);
+        _ ->
+          {[]}
+      end,
+  Messges;
+
+fetch_inbox(Host, User, DB_Type) ->
+  throw({error, iolist_to_binary(io_lib:format("Unsupported backend: ~p",
+             [DB_Type]))}).
+
+
+makeQuery(User) ->
+  [<<"select bare_peer, txt, id, created_at from (
+select bare_peer, txt, created_at, id, rank() over (partition by bare_peer order by timestamp desc)
+FROM archive WHERE username='">> , User , <<"' ) yo
+WHERE rank=1 order by created_at desc">>].
+
+
+
+
+
